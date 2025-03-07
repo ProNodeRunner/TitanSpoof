80f7cec3aacf9a3cfb68c937721bf2f1c936d75d
ProNodeRunner <pronoderunner@gmail.com>
2025-03-08 06:28:13 +0700

    Update Titan.sh

--- Titan.sh
+++ Titan.sh
@@ -118,14 +118,12 @@ ENV PROXY_PASS=${proxy_pass}
 
 RUN apt update && \
     DEBIAN_FRONTEND=noninteractive apt install -y proxychains4 curl && \
-    echo "proxychains4 proxychains4.conf boolean false" | debconf-set-selections && \
-    rm -f /etc/proxychains4.conf && \
     echo "strict_chain" > /etc/proxychains4.conf && \
     echo "proxy_dns" >> /etc/proxychains4.conf && \
     echo "tcp_read_time_out 15000" >> /etc/proxychains4.conf && \
     echo "tcp_connect_time_out 8000" >> /etc/proxychains4.conf && \
     echo "[ProxyList]" >> /etc/proxychains4.conf && \
-    echo "socks5 ${PROXY_HOST} ${PROXY_PORT} ${PROXY_USER} ${PROXY_PASS}" >> /etc/proxychains4.conf
+    echo "socks5 ${proxy_host} ${proxy_port} ${proxy_user} ${proxy_pass}" >> /etc/proxychains4.conf
 EOF
 
 # Собираем кастомный образ
