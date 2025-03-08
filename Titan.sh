bbfab2bef8d0a0a45dfce630e67c87743702c7c4
ProNodeRunner <pronoderunner@gmail.com>
2025-03-09 01:09:22 +0700

    Update Titan.sh

--- Titan.sh
+++ Titan.sh
@@ -45,6 +45,7 @@ show_menu() {
 # (1) Установка компонентов
 ###############################################################################
 install_dependencies() {
+    set -x  # Включаем режим отладки
     echo -e "${ORANGE}[1/7] Инициализация системы...${NC}"
     export DEBIAN_FRONTEND=noninteractive
     export NEEDRESTART_MODE=a  
